80f72eb529a04fc0ef687ae523a126fe51664288
Julio Noguera <noguerajulio24@gmail.com>
2021-03-20 17:52:26 -0500

    update

--- lib/pages/directions_page.dart
+++ lib/pages/directions_page.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qrapp/providers/scan_list_provider.dart';
+import 'package:qrapp/widgets/scan_tiles.dart';
 
 class DirectionsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-
-    final scans = scanListProvider.scans;
-
-    print(scans);
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title: Text(scans[i].value),
-          subtitle: Text(scans[i].id.toString()),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print('hola mundo')),
-    );
+    return ScanTiles(type: 'http');
   }
 }
--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,35 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:qrapp/providers/scan_list_provider.dart';
+import 'package:qrapp/widgets/scan_tiles.dart';
 
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final scanListProvider = Provider.of<ScanListProvider>(context);
-
-    final scans = scanListProvider.scans;
-
-    print(scans);
-
-    return ListView.builder(
-      itemCount: scans.length,
-      itemBuilder: (_, i) => Dismissible(
-        key: UniqueKey(),
-        background: Container(
-          color: Colors.red,
-        ),
-        movementDuration: Duration(milliseconds: 1000),
-        onDismissed: (DismissDirection direction) {
-          Provider.of<ScanListProvider>(context, listen: false)
-              .deleteById(scans[i].id!);
-        },
-        child: ListTile(
-            leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-            title: Text(scans[i].value),
-            subtitle: Text(scans[i].id.toString()),
-            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-            onTap: () => print('hola mundo')),
-      ),
-    );
+    return ScanTiles(type: 'geo');
   }
 }
