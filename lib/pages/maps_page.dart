a89be48d0619b6b9dacf2aa298ff09432f31122f
Julio Noguera <noguerajulio24@gmail.com>
2021-03-20 16:38:21 -0500

    add listview in maps

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class MapsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Hola perros"),
-      ),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (_, i) => ListTile(
+          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+          title: Text('http://facebook.com'),
+          subtitle: Text('ID: 1'),
+          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+          onTap: () => print('hola mundo')),
     );
   }
 }
