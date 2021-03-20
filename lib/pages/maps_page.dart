c0b0d305463054312c14611a8b9f890b2d6b28b0
Julio Noguera <noguerajulio24@gmail.com>
2021-03-20 17:43:12 -0500

    update

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -13,12 +13,23 @@ class MapsPage extends StatelessWidget {
 
     return ListView.builder(
       itemCount: scans.length,
-      itemBuilder: (_, i) => ListTile(
-          leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
-          title: Text(scans[i].value),
-          subtitle: Text(scans[i].id.toString()),
-          trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
-          onTap: () => print('hola mundo')),
+      itemBuilder: (_, i) => Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+        ),
+        movementDuration: Duration(milliseconds: 1000),
+        onDismissed: (DismissDirection direction) {
+          Provider.of<ScanListProvider>(context, listen: false)
+              .deleteById(scans[i].id!);
+        },
+        child: ListTile(
+            leading: Icon(Icons.map, color: Theme.of(context).primaryColor),
+            title: Text(scans[i].value),
+            subtitle: Text(scans[i].id.toString()),
+            trailing: Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+            onTap: () => print('hola mundo')),
+      ),
     );
   }
 }
