87be4bac36c90d56c7cb8b38c190193185bb5e28
Julio Noguera <noguerajulio24@gmail.com>
2021-03-20 19:45:37 -0500

    add map

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,18 +1,37 @@
 // @dart=2.9
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:qrapp/providers/db_provider.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
+    final CameraPosition initialPosition = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Maps'),
       ),
-      body: Center(
-        child: Text(scan.value),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
