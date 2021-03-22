78164f6b4b34e580999a861ce99ba62a1d35f3d0
Julio Noguera <noguerajulio24@gmail.com>
2021-03-22 16:24:38 -0500

    change Icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,14 @@ import 'package:qrapp/pages/map_page.dart';
 import 'package:qrapp/providers/scan_list_provider.dart';
 import 'package:qrapp/providers/ui_provider.dart';
 
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
